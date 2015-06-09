c19ee679740713187db3025895f2432dd3260ed6
whitef89 <feher89@gmail.com>
2015-06-09 18:37:03 +0200

    FtpConfigurator GUI

---
+++ FTPConfig/ConfigModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+
+namespace FTPConfig
+{
+    class ConfigModel : INotifyPropertyChanged
+    {
+        #region dataMembers
+
+        private String serverURL;
+        private String userName;
+        private String password;
+        private String proxy;
+        private Collection<string> pathToCopy;
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region constructors
+        public ConfigModel()
+        {
+
+        }
+
+        public ConfigModel(String url, String user, String pass, String proxy, Collection<String> path )
+        {
+            this.serverURL = url;
+            this.userName = user;
+            this.password = pass;
+            this.proxy = proxy;
+            this.pathToCopy = path;
+
+
+        }
+        #endregion
+
+        #region publicProperties
+        public string FTPServerUrl {
+            get{ return this.serverURL;}
+            set {
+
+                this.serverURL = value;
+                this.NotifyPropertyChanged("FTPServerUrl");
+                
+            } 
+        }
+
+        public string UserName
+        {
+            get { return this.userName; }
+            set
+            {            
+                this.userName = value;
+                this.NotifyPropertyChanged("UserName");
+            }
+        }
+
+        public string Password
+        {
+            get { return this.password ;}
+            set
+            {
+                    this.password = value;
+                    this.NotifyPropertyChanged("Password");
+            }
+        }
+
+        public string ProxyUrl
+        {
+            get { return this.proxy; }
+            set
+            {
+                    this.proxy = value;
+                    this.NotifyPropertyChanged("ProxyUrl");
+            }
+        }
+
+        public Collection<string> path
+        {
+            get { return this.pathToCopy; }
+            set
+            {
+                this.pathToCopy = value;
+                this.NotifyPropertyChanged("path");
+            }
+        }
+        #endregion
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public override string ToString()
+        {
+            return "Konfig: " + FTPServerUrl + " " + UserName + " " + Password + " " + ProxyUrl + " " + path;
+        }
+
+
+    }
+}
---
+++ FTPConfig/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FTPConfig
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConfigModel _config = new ConfigModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            this.DataContext = _config;
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(_config);
+        }
+    }
+}
