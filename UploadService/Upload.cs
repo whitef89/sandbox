63a934d2385f81dcb781ccfbd95f090edf19cf37
whitef89 <feher89@gmail.com>
2015-06-16 00:45:12 +0200

    Initialize window service

---
+++ UploadService/Upload.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace UploadService
+{
+    public partial class Upload : ServiceBase
+    {
+        public Upload()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
